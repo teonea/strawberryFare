d0e933f160ed66e2db42013cc92b7cc61569ba04
Jake Ovenden <jake.dale@xtra.co.nz>
2015-03-10 17:37:08 +1300

    created ajax functionality to avoid a page refresh with the menu which would have occurred with the input type="submit" could have possibly lost the viewers place on the page since its a scroller

--- site/js/menuAjax.js
+++ site/js/menuAjax.js
@@ -1,32 +1,20 @@
 $(document).ready(function(){
-	//add event listeners
-	$('#searchField').keyup(predictiveSearch);
-});
-
-function predictiveSearch() {
-	//empty out values
-	$('#searchPredicts').html('');
-	//assign value to a variable
-	var search = $('#searchField').val();
-	if(search == ''){
-		return;
-	}
-	//prepare data to send
-	var dataToServer = {
-		value: search
-	};
-	//use AJAX to communicate with the server
-	$.ajax({
-		type: 'post',
-		url: 'predictiveSearch.php',
-		data: dataToServer,
-		success: function(dataFromServer){
-			$(dataFromServer).each(function(i){
-				$('#searchPredicts').append('<li>'+dataFromServer[i]+'</li>');
-			});	
-		},
-		error: function(){
-			alert('Error');
-		}
+	// //add event listeners
+	$("button").click(function(){
+		//empty out values
+		$('#menu').html('');
+		//assign value to a variable
+		var menuType = $(this).val();
+		//use AJAX to communicate with the server
+		$.ajax({
+			dataType: "json",
+			url: "ajax/"+menuType+".json",
+			success: function(data){
+				$('#menu').append('<h2>'+menuType+'</h2>');
+				$(data).each(function(i){
+					$('#menu').append('<h3>'+data[i].name+'</h3><p>'+data[i].description+'</p><p><em>'+data[i].price+'</em></p>');
+				})
+			}
+		});
 	});
-}
+});
--- site/pages/menu.php
+++ site/pages/menu.php
@@ -1,36 +1,11 @@
 <div>
 	<h1>Menu</h1>
-	<form method="post" action="<?php $_SERVER['REQUEST_URI']?>">
-		<input type="submit" name="menu" value="Breakfast" />
-		<input type="submit" name="menu" value="Starters" />
-		<input type="submit" name="menu" value="Mains" />
-		<input type="submit" name="menu" value="Drinks" />
-		<input type="submit" name="menu" value="Desserts" />	
-	</form>
-
-<!-- 	<?php 
-		// if(isset($_POST['menu'])){
-		// 	switch($_POST['menu']){
-		// 		case 'Breakfast':
-		// 			include('includes/breakfast.php');
-		// 			break;
-		// 		case 'Starters':
-		// 			include('includes/starters.php');
-		// 			break;
-		// 		case 'Mains':
-		// 			include('includes/mains.php');
-		// 			break;
-		// 		case 'Desserts':
-		// 			include('includes/desserts.php');
-		// 			break;
-		// 		case 'Drinks':
-		// 			include('includes/drinks.php');
-		// 			break;
-		// 	}
-		// } else {
-		// 	include('breakfast.php');
-		// }
-	?> -->
+		<button value="Breakfast">Breakfast</button>
+		<button value="Starters">Starters</button>
+		<button value="Mains">Mains</button>
+		<button value="Drinks">Drinks</button>
+		<button value="Desserts">Desserts</button>	
+	<div id="menu"></div>
 </div>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
